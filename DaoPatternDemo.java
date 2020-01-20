4905841ad5315e4298f914c4a768fd843518b2ed
JP <49924656+jp-2019@users.noreply.github.com>
2020-01-20 11:02:33 -0500

    Add files via upload

---
+++ DaoPatternDemo.java
@@ -0,0 +1,19 @@
+public class DaoPatternDemo {
+    public static void main(String[] args) {
+        StudentDao studentDao = new StudentDaoImpl();
+
+        //print all students
+        for (Student student : studentDao.getAllStudents()) {
+            System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+        }
+
+        //update student
+        Student student =studentDao.getAllStudents().get(0);
+        student.setName("Kenny");
+        studentDao.updateStudent(student);
+
+        //get student
+        studentDao.getStudent(0);
+        System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+    }
+}
